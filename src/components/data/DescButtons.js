912572d86f9ca281a6f24712b60ae42bee54b4e6
Akimarin <tatyana011@bk.ru>
2023-09-13 21:52:10 +0500

    changed href

--- src/components/data/DescButtons.js
+++ src/components/data/DescButtons.js
@@ -3,18 +3,18 @@ export const descButtons = [
         name: "Игра",
         color: "secondary",
         icon: "coming soon",
-        url: "/Game",
+        url: "MineSweeper/Game",
     },
     {
         name: "Таблица лидеров",
         color: "primary",
         icon: "coming soon",
-        url: "/Leaders",
+        url: "MineSweeper/Leaders",
     },
     {
         name: "Правила игры",
         color: "primary",
         icon: "coming soon",
-        url: "/Rules",
+        url: "MineSweeper/Rules",
     },
 ]
